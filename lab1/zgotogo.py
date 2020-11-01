3f554dda74df94a88d8330c87832ac85f73d84c0
danilapal <danilapal@mail.ru>
2020-11-01 22:20:07 +0300

    minor fix

--- lab1/zgotogo.py
+++ lab1/zgotogo.py
@@ -12,7 +12,6 @@ class turtlebot():
         self.velocity_publisher = rospy.Publisher('/turtle_2/cmd_vel', Twist, queue_size=10)
         self.pose_subscriber = rospy.Subscriber('/turtle_2/pose', Pose, self.callback)
 	self._subscriber = rospy.Subscriber('/turtle_1/pose', Pose, self.move2goal)
-	self.initPose = 1
         self.pose = None
         self.rate = rospy.Rate(10)
 
